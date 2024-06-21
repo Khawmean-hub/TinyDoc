0b7922f3691d0d2c83fa5b9cd4432152b7f92cde
daraboth <daraboth0331@gmail.com>
2024-06-21 14:42:02 +0900

    no-cors

--- js/other/generative-ai.js
+++ js/other/generative-ai.js
@@ -1,11 +1,12 @@
 // ------------------------------------------------------------------
-String.prototype.ask = () => {
+function ask(){
   const url = "https://www.vongpichdaraboth.net/ai/message?message=" + this;
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
     },
+    mode: 'no-cors'
   };
 
   return new Promise((resolve, reject) => {
