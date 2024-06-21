998843047e6750fecfdd3ef128eff91baac59c5f
daraboth <daraboth0331@gmail.com>
2024-06-21 14:36:59 +0900

    add ask

---
+++ js/other/generative-ai.js
@@ -0,0 +1,17 @@
+// ------------------------------------------------------------------
+String.prototype.ask = () => {
+  const url = "https://www.vongpichdaraboth.net/ai/message?message=" + this;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+    },
+  };
+
+  return new Promise((resolve, reject) => {
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
+}
